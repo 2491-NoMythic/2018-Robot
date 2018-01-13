1086cd3575738bd1d80e099e6bc3e3f0dbb5eb31
Silas <hwalker963@gmail.com>
2018-01-13 13:59:03 -0600

    made the killswitch not retarded

--- src/com/_2491nomythic/robot/commands/KillSwitch.java
+++ src/com/_2491nomythic/robot/commands/KillSwitch.java
@@ -21,7 +21,7 @@ public class KillSwitch extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
