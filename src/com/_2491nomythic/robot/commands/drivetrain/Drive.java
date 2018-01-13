c80f073e80b6ad4eff585d62452f819a9cabf828
Adrian <awkirchner@gmail.com>
2018-01-13 09:32:21 -0600

    fixed an error in drive

--- src/com/_2491nomythic/robot/commands/drivetrain/Drive.java
+++ src/com/_2491nomythic/robot/commands/drivetrain/Drive.java
@@ -69,7 +69,6 @@ public class Drive extends CommandBase {
     // Called once after isFinished returns true
     protected void end() {
     	drivetrain.stop();
-    	timer.stop();
     }
 
     // Called when another command which requires one or more of the same
