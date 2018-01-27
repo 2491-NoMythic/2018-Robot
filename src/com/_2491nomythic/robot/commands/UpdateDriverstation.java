1189c9b9bfb38cdf2f90937aec539886f80d37f5
Adrian <awkirchner@gmail.com>
2018-01-27 13:10:58 -0600

    String spacing

--- src/com/_2491nomythic/robot/commands/UpdateDriverstation.java
+++ src/com/_2491nomythic/robot/commands/UpdateDriverstation.java
@@ -42,8 +42,8 @@ public class UpdateDriverstation extends CommandBase {
 			SmartDashboard.putNumber("RightEncoder", drivetrain.getRightEncoderDistance());
 			SmartDashboard.putNumber("Avg. Encoder Distance", drivetrain.getDistance());
 			SmartDashboard.putNumber("Current PID Input", drivetrain.returnPIDInput());
-			SmartDashboard.putNumber("LeftShooter Speed", shooter.getLeftShootEncoder());
-			SmartDashboard.putNumber("RightShooter Speed", shooter.getRightShootEncoder());
+			SmartDashboard.putNumber("Left Shooter Speed", shooter.getLeftShootEncoder());
+			SmartDashboard.putNumber("Right Shooter Speed", shooter.getRightShootEncoder());
 		}
 			
     	
