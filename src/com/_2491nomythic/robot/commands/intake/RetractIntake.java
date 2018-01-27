d02be14fc14ee478ff36924c47e1a57d235d56be
Adrian <awkirchner@gmail.com>
2018-01-27 11:11:17 -0600

    Added Pneumatics contol commands for the intake

---
+++ src/com/_2491nomythic/robot/commands/intake/RetractIntake.java
@@ -0,0 +1,41 @@
+package com._2491nomythic.robot.commands.intake;
+
+import com._2491nomythic.robot.commands.CommandBase;
+
+/**
+ * Retracts the shooter into the robot
+ */
+public class RetractIntake extends CommandBase {
+
+	/**
+	 * Retracts the shooter into the robot
+	 */
+    public RetractIntake() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(intake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	intake.retract();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
