2179f95645ab6258e5c1fc980a84e7e3b6a8c988
gballen <35871516+gballen@users.noreply.github.com>
2018-01-30 19:47:48 -0600

    Changed a thing
    Yeah

--- src/com/_2491nomythic/robot/commands/shooter/ToggleShooterPosition.java
+++ src/com/_2491nomythic/robot/commands/shooter/ToggleShooterPosition.java
@@ -31,7 +31,7 @@ public class ToggleShooterPosition extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
