b6cbe99fb464072eac77c4efde373809f72303f7
gballen <35871516+gballen@users.noreply.github.com>
2018-02-01 17:24:25 -0600

    Matched Constants to Actual Numbers
    Constants.java values now correspond to actual Talon numbers.

--- src/com/_2491nomythic/robot/settings/Constants.java
+++ src/com/_2491nomythic/robot/settings/Constants.java
@@ -6,31 +6,29 @@ package com._2491nomythic.robot.settings;
 public class Constants {
 	
 	//Drive
-	public static final int driveTalonLeft1Channel = 11;
-	public static final int driveTalonLeft2Channel = 19;
-	public static final int driveTalonLeft3Channel = 14;
-	public static final int driveTalonRight1Channel = 12;
-	public static final int driveTalonRight2Channel = 15;
-	public static final int driveTalonRight3Channel = 13;
+	public static final int driveTalonLeft1Channel = 1;
+	public static final int driveTalonLeft2Channel = 2;
+	public static final int driveTalonRight1Channel = 3;
+	public static final int driveTalonRight2Channel = 4;
 	public static final int driveEncoderLeftChannel1 = 3;
 	public static final int driveEncoderLeftChannel2 = 4;
-	public static final int driveEncoderCenterChannel1 = 5;
-	public static final int driveEncoderCenterChannel2 = 6;
 	public static final int driveEncoderRightChannel1 = 7;
 	public static final int driveEncoderRightChannel2 = 8;
 	
 	//Intake
-	public static final int intakeTalonLeftChannel = 2491; //TODO Change this to an actual talon
-	public static final int intakeTalonRightChannel = 2491; //TODO Change this to an actual talon
-	public static final int intakeTalonBottomChannel = 2491; //TODO Change this to an actual talon
+	public static final int intakeTalonLeft1Channel = 5;
+	public static final int intakeTalonLeft2Channel = 6;
+	public static final int intakeTalonRight1Channel = 7;
+	public static final int intakeTalonRight2Channel = 8;
+	public static final int intakeTalonBottomChannel = 9;
 	public static final int intakeSolenoidActivateChannel = 2491; //TODO Change this to an actual solenoid channel.
 	public static final int intakeSolenoidOpenChannel = 2491; //TODO Change this to an actual solenoid channel.
 
 	//Shooter
-	public static final int shooterTalonLeftAccelerateChannel = 2491; //TODO Change this to an actual talon
-	public static final int shooterTalonLeftShootChannel = 2491; //TODO Change this to an actual talon
-	public static final int shooterTalonRightAccelerateChannel = 2491; //TODO Change this to an actual talon
-	public static final int shooterTalonRightShootChannel = 2491; //TODO Change this to an actual talon
+	public static final int shooterTalonLeftAccelerateChannel = 12;
+	public static final int shooterTalonLeftShootChannel = 14;
+	public static final int shooterTalonRightAccelerateChannel = 13;
+	public static final int shooterTalonRightShootChannel = 15;
 	public static final int shooterElevatorChannel = 12;
 	public static final int timeForShooterToSpinUp = 2;
 	public static final int timeForShooterToRaise = 4;
@@ -43,8 +41,8 @@ public class Constants {
 	public static final int lightIC2Channel = 1;
 	
 	//CubeStorage
-	public static final int cubeStorageTalonLeftChannel = 2491; //TODO Change this to an actual talon
-	public static final int cubeStorageTalonRightChannel = 2491; //TODO Change this to an actual talon
+	public static final int cubeStorageTalonLeftChannel = 10;
+	public static final int cubeStorageTalonRightChannel = 11;
 		
 	//Computation
 	public static final double driveEncoderToInches = 1.066 * 2.4 * 1200.0 / 670 / 256; //TODO update for 2018. Currently in 2017 inches.
