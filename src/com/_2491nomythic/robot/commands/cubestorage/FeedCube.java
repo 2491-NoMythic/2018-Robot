a578389e4cbe744e275d7a4d8b96894b827cfca0
Silas <hwalker963@gmail.com>
2018-02-03 14:03:08 -0600

    Properly spaced (tabbed) FeedCube

--- src/com/_2491nomythic/robot/commands/cubestorage/FeedCube.java
+++ src/com/_2491nomythic/robot/commands/cubestorage/FeedCube.java
@@ -7,37 +7,38 @@ import com._2491nomythic.robot.commands.CommandBase;
  */
 public class FeedCube extends CommandBase {
 
-    /**
-     * Feeds cube from cube storage to shooter.
-     */
+	/**
+	 * Feeds cube from cube storage to shooter.
+	 */
+	
 	public FeedCube() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(cubeStorage);
-    }
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(cubeStorage);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	cubeStorage.run(1);
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		cubeStorage.run(1);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return true;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	cubeStorage.stop();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		cubeStorage.stop();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
