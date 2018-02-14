e88656b698e8686db9e3e2a88501999bd983fe14
Thomas <thetardis1968@gmail.com>
2018-02-14 16:32:14 -0600

    Made RunIntakeManual also run the CubeStorage

--- src/com/_2491nomythic/robot/commands/intake/RunIntakeManual.java
+++ src/com/_2491nomythic/robot/commands/intake/RunIntakeManual.java
@@ -15,6 +15,7 @@ public class RunIntakeManual extends CommandBase {
 		// Use requires() here to declare subsystem dependencies
 		// eg. requires(chassis);
 		requires(intake);
+		requires(cubeStorage);
 	}
 
 	// Called just before this Command runs the first time
@@ -24,6 +25,7 @@ public class RunIntakeManual extends CommandBase {
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		intake.run(oi.getAxisDeadzonedSquared(ControllerMap.driveController, ControllerMap.intakeAxis, 0.05));
+		cubeStorage.run(oi.getAxisDeadzonedSquared(ControllerMap.operatorController, ControllerMap.intakeAxis, 0.05));
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
