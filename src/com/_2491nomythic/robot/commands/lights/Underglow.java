8fdb4ff590f7e0b0db5c9ac6711d100e1ead44d2
Silas <hwalker963@gmail.com>
2018-02-14 19:19:43 -0600

    changed stuff in underglow to the right stuff
    also set it up to work as a default command in the future

--- src/com/_2491nomythic/robot/commands/lights/Underglow.java
+++ src/com/_2491nomythic/robot/commands/lights/Underglow.java
@@ -2,8 +2,6 @@ package com._2491nomythic.robot.commands.lights;
 
 import com._2491nomythic.robot.commands.CommandBase;
 import com._2491nomythic.robot.settings.Constants;
-import com._2491nomythic.robot.subsystems.SickLights;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
@@ -16,21 +14,21 @@ public class Underglow extends CommandBase {
     public Underglow() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(sickLights);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	Alliance teamColor = DriverStation.getInstance().getAlliance();
-    	String teamColorString = teamColor.toString();
     	if(teamColor == DriverStation.Alliance.Red) {
-    		SickLights.getInstance().writeSignal(25, Constants.underglowPWM);
+    		sickLights.writeSignal(25, Constants.underglowPWM);
     	}
     	else if (teamColor == DriverStation.Alliance.Blue) {
-    		SickLights.getInstance().writeSignal(100, Constants.underglowPWM);
+    		sickLights.writeSignal(100, Constants.underglowPWM);
     	}
     	else
     	{
-    		SickLights.getInstance().writeSignal(255, Constants.underglowPWM);
+    		sickLights.writeSignal(255, Constants.underglowPWM);
     	}
     	
     }
