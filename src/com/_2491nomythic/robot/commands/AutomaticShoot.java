155fc719533753dd5a61fb1b532310534f31a2c3
Silas <hwalker963@gmail.com>
2018-02-15 19:51:55 -0600

    Made AutomaticShoot also end based on power cube held status

--- src/com/_2491nomythic/robot/commands/AutomaticShoot.java
+++ src/com/_2491nomythic/robot/commands/AutomaticShoot.java
@@ -97,7 +97,7 @@ public class AutomaticShoot extends CommandBase {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return timer.get() > Constants.timeForShooterToFire;
+		return timer.get() > Constants.timeForShooterToFire && !cubeStorage.isHeld();
 	}
 
 	// Called once after isFinished returns true
