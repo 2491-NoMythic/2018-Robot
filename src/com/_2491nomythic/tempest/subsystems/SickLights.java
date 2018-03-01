9f372941e6c23f42fd9c1fb226c5a29f839bca27
Thomas <thetardis1968@gmail.com>
2018-02-28 22:00:38 -0600

    single solenoid lights

--- src/com/_2491nomythic/tempest/subsystems/SickLights.java
+++ src/com/_2491nomythic/tempest/subsystems/SickLights.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class SickLights extends Subsystem {
 	private static SickLights instance;
-	private Solenoid shooter1, shooter2;
+	private Solenoid activator;
 
 	public static SickLights getInstance() {
 		if(instance == null) {
@@ -21,40 +21,15 @@ public class SickLights extends Subsystem {
 	 * The system which controls the data sent to the lights.
 	 */
 	public SickLights() {
-		//shooter1 = new Solenoid(4);
-		//shooter2 = new Solenoid(5);
+		//activator = new Solenoid(4);
 	}
 	
 	/**
-	 * Updates the solenoids to be recognized by the Arduino.
-	 * @param pattern Which light pattern to use from 1-4.
-	 * Pattern 1: Static Purple
-	 * Pattern 2: Shooter Spinning Up
-	 * Pattern 3: Ready to Fire
-	 * Pattern 4: Picked up Cube (Detected by ultrasonic)
+	 * Sets the state of the lights.
+	 * @param state True for on, false for off.
 	 */
-	public void writePattern(int pattern) {
-		switch(pattern) {
-		case 1:
-			shooter1.set(false);
-			shooter2.set(false);
-			break;
-		case 2:
-			shooter1.set(true);
-			shooter2.set(false);
-			break;
-		case 3:
-			shooter1.set(false);
-			shooter2.set(true);
-			break;
-		case 4:
-			shooter1.set(true);
-			shooter2.set(true);
-			break;
-		default:
-			System.out.println("Invalid pattern in SickLights. Pattern: " + pattern);
-			break;
-		}
+	public void set(boolean state) {
+		//activator.set(state);
 	}
 	
 	public void initDefaultCommand() {
