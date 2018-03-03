6293a6bfdfe8178a6fe8b5182bfd9c161dc450f8
Silas <hwalker963@gmail.com>
2018-03-02 19:35:53 -0600

    Updated SickLights to working single-solenoid model

--- src/com/_2491nomythic/tempest/commands/lights/UpdateLights.java
+++ src/com/_2491nomythic/tempest/commands/lights/UpdateLights.java
@@ -6,66 +6,79 @@ import com._2491nomythic.tempest.settings.Variables;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- * Updates the pattern sent to the lights during Teleop and Autonomous
+ *
  */
 public class UpdateLights extends CommandBase {
-	private Timer timer;
 	private boolean newCube;
+	private Timer timer, sonicTimer;
+	private int state;
 
-	/**
-	 * Updates the pattern sent to the lights during Teleop and Autonomous
-	 */
-	public UpdateLights() {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		requires(sickLights);
-	}
+    public UpdateLights() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	setRunWhenDisabled(true);
+    	timer = new Timer();
+    	sonicTimer = new Timer();
+    }
 
-	// Called just before this Command runs the first time
-	protected void initialize() {
-		newCube = false;
-	}
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer.start();
+    	sonicTimer.start();
+    	state = 0;
+    }
 
-	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
-		if(shooter.getAllMotorVelocity() >= 0.05) {
-			if(Variables.readyToFire) {
-				sickLights.writePattern(3);
-				timer.stop();
-				timer.reset();
-			}
-			else {
-				sickLights.writePattern(2);
-			}
-		}
-		else if(cubeStorage.isHeld() && newCube) {
-			timer.start();
-			sickLights.writePattern(4);
-		}
-		else {
-			sickLights.writePattern(1);
-		}
-		
-		if(timer.get() < 3) {
-			newCube = true;
-		}
-		else {
-			newCube = false;
-		}
-	}
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(cubeStorage.isHeld()) {
+    		if(sonicTimer.get() < 3) {
+    			newCube = true;
+    		}
+    		else {
+    			newCube = false;
+    		}
+    	}
+    	
+    	if(Variables.readyToFire || newCube) {    		
+    		switch(state) {
+    		case 0:
+    			sickLights.set(false);
+    			if(timer.get() > 0.5) {
+    				timer.reset();
+    				state++;
+    			}
+    			break;
+    		case 1:
+    			sickLights.set(true);
+    			if(timer.get() > 0.5) {
+    				timer.reset();
+    				state++;
+    			}
+    			
+    			break;
+    		default:
+    			break;
+    		}
+    	}
+    	else {
+    		sickLights.set(true);
+    		state = 0;
+    		sonicTimer.reset();
+    	}
+    }
 
-	// Make this return true when this Command no longer needs to run execute()
-	protected boolean isFinished() {
-		return false;
-	}
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
 
-	// Called once after isFinished returns true
-	protected void end() {
-	}
+    // Called once after isFinished returns true
+    protected void end() {
+    }
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	protected void interrupted() {
-		end();
-	}
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
 }
