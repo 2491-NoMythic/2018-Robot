eb1c7669cec660518b68730727d67814c91880e0
Silas <hwalker963@gmail.com>
2018-03-18 15:31:43 -0500

    Added the updated reverse shooter

--- src/com/_2491nomythic/tempest/OI.java
+++ src/com/_2491nomythic/tempest/OI.java
@@ -21,6 +21,7 @@ import com._2491nomythic.tempest.commands.intake.RunIntakeManual;
 import com._2491nomythic.tempest.commands.intake.RunIntakeRollerless;
 import com._2491nomythic.tempest.commands.intake.ToggleIntakeDeployment;
 import com._2491nomythic.tempest.commands.intake.ToggleIntakeOpeningHeld;
+import com._2491nomythic.tempest.commands.shooter.ReverseShooter;
 import com._2491nomythic.tempest.commands.shooter.RunShooterManual;
 import com._2491nomythic.tempest.commands.shooter.SetShooterSpeed;
 import com._2491nomythic.tempest.commands.shooter.ToggleShooterPosition;
@@ -121,7 +122,7 @@ public class OI {
 		cubeStorageControl2.whenPressed(new UltrasonicCubeHaltManual());
 		
 		reverseShooter = new JoystickButton(controllers[ControllerMap.operatorController], ControllerMap.shooterReverseButton);
-		//reverseShooter.whileHeld(new ReverseShooterHeld());
+		reverseShooter.whileHeld(new ReverseShooter());
 		
 		runIntakeRollerless = new JoystickButton(controllers[ControllerMap.operatorController], ControllerMap.IntakeRollerlessButton);
 		runIntakeRollerless.whileHeld(new RunIntakeRollerless());
